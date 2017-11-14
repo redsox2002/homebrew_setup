7d652a336d429ff59cd941b67d8e67ffad1ce7fc
Sudhir Ganesan <ganesansudhir18@gmail.com>
2017-11-14 10:31:22 -0500

    Updated script to install cask

--- homebrew_setup.sh
+++ homebrew_setup.sh
@@ -3,6 +3,9 @@
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install caskroom
+brew tap caskroom/cask
+
 # Install brew packages
 brew install $(< brew_list.txt )
 
